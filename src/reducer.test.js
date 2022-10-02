33ec34bb0a428812fa40524f9b7ce6e4b8b3e17d
Leo <m.leo.cen@outlook.com>
2022-10-02 22:06:34 +0800

    feat: 6.1

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -9,12 +9,12 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
+    const state = undefined
     const action = {
       type: 'DO_NOTHING'
     }
 
-    const newState = counterReducer(undefined, action)
+    const newState = counterReducer(state, action)
     expect(newState).toEqual(initialState)
   })
 
@@ -26,8 +26,51 @@ describe('unicafe reducer', () => {
 
     deepFreeze(state)
     const newState = counterReducer(state, action)
+    
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is increamented', () => {
+    const action = { type: 'OK' }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is increamented', () => {
+    const action = { type: 'BAD' }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero button', () => {
+    const action = { type: 'ZERO' }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
